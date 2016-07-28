71fc2bb2d6cfa3bafc081396ba4b3b514c2b5e0a
Nicolás Venturo <nicolas.venturo@gmail.com>
2016-07-28 02:52:01 -0300

    Continued cleaning up the makefile and .c files

--- display/download.sh
+++ display/download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo openocd --file /usr/local/share/openocd/scripts/board/ek-lm4f120xl.cfg --command "program gcc/threesixty-display.axf verify reset exit"
+sudo openocd --file /usr/local/share/openocd/scripts/board/ek-lm4f120xl.cfg --command "program out/threesixty-display.axf verify reset exit"
